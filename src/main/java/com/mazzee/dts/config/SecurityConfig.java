a510014a5dbaf0ca2464c238f9a3a3b41dc5c5dc
ShaikhMazharuddin <mazhar.s2k18@gmail.com>
2022-08-28 16:28:15 +0530

    added privacy policy html

--- src/main/java/com/mazzee/dts/config/SecurityConfig.java
+++ src/main/java/com/mazzee/dts/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 	private static final String[] SWAGGER_URLS = { "/swagger-ui/**", "/swagger-resources/**", "/v3/api-docs" };
-	private static final String[] INSECURE_URLS = { "/api/user/**", "/images/**", "/dts-images/**", "/api/userHelp/**" };
+	private static final String[] INSECURE_URLS = { "/api/user/**", "/images/**", "/dts-images/**", "/html/**", "/api/userHelp/**" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
